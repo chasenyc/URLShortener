14229d637ffa0093aa3eb591fb6bcce474f53499
App Academy Student <appacademy@Apps-Mac-mini-11.local>
2015-10-14 17:37:53 -0400

    Bugfix migrations

--- url-shortener/db/migrate/20151014213023_taggings.rb
+++ url-shortener/db/migrate/20151014213023_taggings.rb
@@ -3,7 +3,8 @@ class Taggings < ActiveRecord::Migration
     create_table :taggings do |t|
       t.integer :short_url_id
       t.integer :tag_topic_id
-      
+
       t.timestamps
+    end
   end
 end
--- url-shortener/db/migrate/20151014213249_tag_topics.rb
+++ url-shortener/db/migrate/20151014213249_tag_topics.rb
@@ -2,7 +2,8 @@ class TagTopics < ActiveRecord::Migration
   def change
     create_table :tag_topics do |t|
       t.string :tag
-      
+
       t.timestamps
+    end
   end
 end
