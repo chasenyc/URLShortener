99b73827b9dc9fc5752e537e077ba8c49e5a2c81
App Academy Student <appacademy@Apps-Mac-mini-11.local>
2015-10-14 14:13:53 -0400

    Add shortened_urls table

--- url-shortener/db/schema.rb
+++ url-shortener/db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151014175426) do
+ActiveRecord::Schema.define(version: 20151014180810) do
+
+  create_table "shortened_urls", force: true do |t|
+    t.string   "long_url"
+    t.string   "short_url"
+    t.integer  "submitter_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email"
