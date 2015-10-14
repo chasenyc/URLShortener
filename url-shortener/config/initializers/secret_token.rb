3e7c0fb23ded73cc844c8e65245fb130457f28e8
App Academy Student <appacademy@Apps-Mac-mini-11.local>
2015-10-14 13:46:08 -0400

    Generate rails.

---
+++ url-shortener/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UrlShortener::Application.config.secret_key_base = '8c42296ba6523bd0da0c61cf963eb8b3ead7fb59cdf33eeda34ee6a04bd31489a1695e62c197858b8bdd563c3de18d6f7f957b1abf102ff0bc33bb3effc48629'
